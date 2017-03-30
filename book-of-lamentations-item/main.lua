f7bcbb6b408e7b4312195de2a68eed0df565c238
Sylvain MONTEILLET <monteillet.s@mipih.fr>
2017-03-30 09:15:31 +0200

    [BoL] add tears up on use

--- book-of-lamentations-item/main.lua
+++ book-of-lamentations-item/main.lua
@@ -1,12 +1,28 @@
-local bookOfLamentations = RegisterMod("BookOfLamentations",1)
+local bookMod = RegisterMod("BookOfLamentations",1)
+local bookItem = Isaac.GetItemIdByName("Book of lamentations")
 
-local bookOfLamentationsItem = Isaac.GetItemIdByName("Book of lamentations")
+bookMod.debug=false
+bookMod.debugStr="Init"
 
-function bookOfLamentations:UseBook()
+function bookMod:useBook()
 	local player = Isaac.GetPlayer(0)
-	if cacheFlag == CacheFlag.CACHE_FIREDELAY then
-	 	player.FireDelay = player.FireDelay - 3;
+   	player:AddCacheflag(CacheFlag.CACHE_FIREDELAY)
+	player:EvaluateItems()
+	return true
+end
+
+function bookMod:updateStats()
+	if cacheFlag == CacheFlags.CACHE_FIREDELAY then
+        player.MaxFireDelay = player.MaxFireDelay - 3
+    end
+end
+
+function mod:debug()
+	if bookMod.debug then
+		Isaac.RenderText(bookMod.debugStr, 50, 15, 255, 255, 255, 255)
 	end
 end
 
-bookOfLamentations:AddCallback(ModCallbacks.MC_USE_ITEM, bookOfLamentations.UseBook, bookOfLamentationsItem)
+bookMod:AddCallback(ModCallbacks.MC_POST_UPDATE, bookMod.debug)
+bookMod:AddCallback(ModCallbacks.MC_USE_ITEM, bookMod.useBook, bookItem)
+bookMod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, bookMod.updateStats)
