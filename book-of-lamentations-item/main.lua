16f2f07865278b421bfd45e22058f9cd3d760ac6
Sylvain Monteillet <sylvain.monteillet@gmail.com>
2017-03-31 23:11:36 +0200

    [BoL] replace DD by AD when book is used in boss fight

--- book-of-lamentations-item/main.lua
+++ book-of-lamentations-item/main.lua
@@ -4,32 +4,61 @@ local mod = RegisterMod("BookOfLamentations",1)
 local bookOfLamentations = {
 	itemID = Isaac.GetItemIdByName("Book of lamentations");
 	isActive = false;
+	debugStr="nothing"
 }
 
 
 function mod:useItem(CollectibleType, RNG)
-	local player = Isaac.GetPlayer(0)
-	player:AddCacheFlags(CacheFlag.CACHE_FIREDELAY)
-	player:EvaluateItems()
-	bookOfLamentations.isActive = true;
+	bookOfLamentations.debugStr="1"
+	if player:HasCollectible(bookOfLamentations.itemID) then
+		local player = Isaac.GetPlayer(0)
+		player:AddCacheFlags(CacheFlag.CACHE_FIREDELAY)
+		player:EvaluateItems()
+		bookOfLamentations.isActive = true
+		bookOfLamentations.debugStr="2"
+		if mod:replaceDevilDealByAngelDealIfPossible() then
+			Game():GetLevel():AddAngelRoomChance(1.0)
+		end
+	end
 	return true
+end
+
+function mod:replaceDevilDealByAngelDealIfPossible(stage)
+	local stage = Game():GetLevel():GetStage()
+	local isBossFight = Game():GetRoom():IsCurrentRoomLastBoss()
+	return isBossFight and (
+		stage == LevelStage.STAGE1_2 or
+		stage == LevelStage.STAGE2_1 or
+		stage == LevelStage.STAGE2_2 or
+		stage == LevelStage.STAGE3_1 or
+		stage == LevelStage.STAGE3_2 or
+		stage == LevelStage.STAGE4_1 or
+		stage == LevelStage.STAGE4_2 or
+		stage == LevelStage.STAGE1_GREED or
+		stage == LevelStage.STAGE2_GREED or
+		stage == LevelStage.STAGE3_GREED or
+		stage == LevelStage.STAGE4_GREED or
+		stage == LevelStage.STAGE5_GREED or
+		stage == LevelStage.STAGE6_GREED
+	)
 end
 
 function mod:evaluateCache(player, cacheFlag)
 	if cacheFlag == CacheFlag.CACHE_FIREDELAY and
 	   player:HasCollectible(bookOfLamentations.itemID) and
-		 bookOfLamentations.isActive then
+	   bookOfLamentations.isActive then
 		player.MaxFireDelay = player.MaxFireDelay - 3;
 	end
 end
 
 function mod:postUpdate()
+	Isaac.RenderText("DebugStr: "..bookOfLamentations.debugStr, 50, 30, 255, 255, 255, 255)
 	if mod:hasPlayerJustEnterARoom() and bookOfLamentations.isActive then
-    bookOfLamentations.isActive = false;
-    local player = Isaac.GetPlayer(0);
-    player:AddCacheFlags(CacheFlag.CACHE_FIREDELAY);
-    player:EvaluateItems();
-  end
+		bookOfLamentations.isActive = false;
+		local player = Isaac.GetPlayer(0);
+		player:AddCacheFlags(CacheFlag.CACHE_FIREDELAY);
+		player:EvaluateItems();
+	end
 end
 
 function mod:hasPlayerJustEnterARoom()
